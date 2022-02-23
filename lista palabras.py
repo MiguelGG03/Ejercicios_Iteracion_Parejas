5edb6c986322825cea92244c783b95f78c06dedd
Nachopedrero <93646581+Nachopedrero@users.noreply.github.com>
2022-02-23 20:15:43 +0100

    Add files via upload

---
+++ lista palabras.py
@@ -0,0 +1,40 @@
+diccionariowords =[]
+listaenorden = ()
+numeroantes = rango(palabranterior)
+rangopalabranterior = rango(listaorden) - 1 
+rangonumeronext = rango(listaorden) + 1
+def posicion_palabra_anterior:
+
+
+
+def posicion_palabra_siguiente:
+
+
+palabra(0)= cualquier palabra que queramos( primera columna)
+if diccionariowords.append x:
+    for palabra in diccionariowords:
+        def posicion_palabra_siguiente:
+        def posicion_palabra_anterior:
+    palabra(1).append( posicion_palabra_siguiente:))
+    palabra(2)
+ # el diccionario ha de tener tres columnas, una la palabra, otra el numero en el que se encuentra la 
+#siguiente palabra en orden alfabetico, y otra para la posicion de la palabra anterior(alfabeticamente
+# ). para saber que numero colocar en cada columna pasamos todas las palabras a una lista, la cual 
+#con un metodo vamos a ordenar alfabeticamente (sort). vamos a pedirle al ordenador que nos diga en que 
+#posicion esta esa palabra y a continuacion (sabiendo la posicion)le preguntaremos que cuales son la
+#anterior y posterior palabra, para luego volver al diccionario y pedir su posicion (de la ant y post),
+#  la cual sera el numero que ocupara estas columnas.
+
+#Para el algoritmo de dar las palabras que empiezan por una letra dad podemos usar :
+# for palabra in lista:
+#    for i in palabra:
+#     if i == letra and rg i == 0 (es decir, i es la primera letra):
+#     print (palabra)
+
+
+# para añadir o eliminar una palabra solo debemos volver a colocar todas las palabras en la lista ordenada
+#y hacer el mismo proceso para saber que numero colocar en cada columna, ya que este numero cambiara en 
+# caso de que modifiquemos el diccionario.
+#Digamos que primero introduces la palabra en el diccionario y luego te encargas de rellenar
+# las otra dos columnas haciendo lo de la lista para saber donde estan la anterior y posterior y actualizas 
+# el diccionario.
