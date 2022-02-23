9b2e1e3cec0adb394d358c017c0f4895f9dc1dbe
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-02-23 16:33:40 +0100

    programa hecho pero no me imprime la lista

--- Ejercicio 6/ejercicio6.py
+++ Ejercicio 6/ejercicio6.py
@@ -1,5 +1,6 @@
 class cuenta:
     saldo = 0
+    historial=[]
     def __init__(self, nombre):
         self.nombre=nombre
 
@@ -12,7 +13,32 @@ class cuenta:
         c.saldo=c.saldo-debito
     def consultar(c):
         return c.saldo
-    def es_acreedora(c):
-        return (c.saldo>=0)
-    def es_deudora(c):
-        return (c.saldo<0)
+    def guardar_historial(c):
+        c.historial.append(str(c.saldo))
+    def mostrar_historial(c):
+        return c.historial[c]
+
+def que_operacion_hacer(h):
+    if(h==1):
+        ingreso=float(input('Cuanto dinero desea abonar?:'))
+        cu.abonar(ingreso)
+    if(h==2):
+        cargo=float(input('Cuanto dinero desea cargar?:'))
+        cu.cargar(cargo)
+
+
+n=str(input('Nombre de la cuenta:'))
+cu = cuenta(n)
+s_i= float(input('Cual es el saldo inicial de la cuenta?:'))
+cu.abrir(s_i)
+fin=False
+while(fin==False):
+    p1=str(input('Quieres seguir operando([S]/[N])?:'))
+    if(p1=='s' or p1 == 'S'):
+        p2=int(input('Que operacion desea hacer?\n (1)-Abonar\n (2)-Cargar\n-'))
+        que_operacion_hacer(p2)
+        cu.guardar_historial
+    if(p1=='n' or p1 == 'N'):
+        fin=True
+print("Sus acciones han sido las siguientes:")
+print(cu.mostrar_historial)
