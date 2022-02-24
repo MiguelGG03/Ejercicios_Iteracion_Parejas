78df8a43d8aa77528c15fa5ced3fe1deb26fcf03
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-02-24 08:57:55 +0100

    pequeño arreglo

--- Ejercicio 8/ejercicio8.py
+++ Ejercicio 8/ejercicio8.py
@@ -1,4 +1,4 @@
-frase=str(input('Introduzca la frase que quiere separar utilizando (:) como separador:\n·'))
+frase=str(input('Introduzca la frase que quiere separar utilizando (:) como separador\n·'))
 palabras=frase.split(sep=':')
 for i in palabras:
     print(i)
